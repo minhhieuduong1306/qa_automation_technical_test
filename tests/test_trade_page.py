9b691afdc6f355acb667e473e019032fd9344215
minhhieuduong1306 <minhhieu.duong1306@gmail.com>
2025-07-28 05:32:07 +0700

    Write test cases for open positions and update place order test cases

--- tests/test_trade_page.py
+++ tests/test_trade_page.py
@@ -9,21 +9,57 @@ class TestTradePage():
       self.driver = driver
       self.login_page = LoginPage(self.driver)
 
-   def test_place_order_by_price(self):
+   def test_place_market_buy_order_by_price(self):
       trade_page = self.login_page.navigate_to_trade()
-      trade_page.filter_markets("Top Gainer")
-      trade_page.select_market("XAUUSD")
+      trade_page.filter_markets("All")
+      trade_page.select_market("GBPAUD")
       trade_page.select_order_type("Market")
-      trade_page.volume = 10
-      trade_page.stop_loss_price = 3333.5
-      trade_page.take_profit_price = 3340.5
-      # trade_page.place_order() # Disabled due to "Market Closed" or "Stay Tuned" status
+      trade_page.volume = 1
+      trade_page.stop_loss_price = 2.0
+      trade_page.take_profit_price = 2.1
+      trade_page.place_buy_order()
+      trade_page.confirm_order()
+      trade_page.verify_order_notification_message('Market Order Submitted')
 
-   def test_place_order_by_points(self):
+   def test_place_market_buy_order_by_points(self):
       trade_page = self.login_page.navigate_to_trade()
-      trade_page.filter_markets("Top Gainer")
-      trade_page.select_market("XAGUSD")
+      trade_page.filter_markets("All")
+      trade_page.select_market("AUDCAD")
       trade_page.select_order_type("Market")
       trade_page.volume = 10
-      trade_page.stop_loss_points = 350
-      trade_page.take_profit_points = 350
+      trade_page.stop_loss_points = 1000
+      trade_page.take_profit_points = 1000
+      trade_page.place_buy_order()
+      trade_page.confirm_order()
+      trade_page.verify_order_notification_message('Market Order Submitted')
+
+   def test_edit_open_position(self):
+      trade_page = self.login_page.navigate_to_trade()
+      trade_page.filter_markets("All")
+      trade_page.select_market("AUDCAD")
+      trade_page.open_positions()
+      trade_page.edit_open_positions(1) # The first open position in the list
+      trade_page.edit_stop_loss_points = 500
+      trade_page.edit_take_profit_points = 500
+      trade_page.update_buy_order()
+      trade_page.confirm_update_order()
+      trade_page.verify_order_notification_message('Market Order Updated')
+   
+   def test_partial_close_open_position(self):
+      trade_page = self.login_page.navigate_to_trade()
+      trade_page.filter_markets("All")
+      trade_page.select_market("AUDCAD")
+      trade_page.open_positions()
+      trade_page.close_open_positions(1) # The first open position in the list
+      trade_page.close_order_volume = 5
+      trade_page.close_order()
+      trade_page.verify_order_notification_message('Close Order')
+
+   def test_close_open_position(self):
+      trade_page = self.login_page.navigate_to_trade()
+      trade_page.filter_markets("All")
+      trade_page.select_market("AUDCAD")
+      trade_page.open_positions()
+      trade_page.close_open_positions(1) # The first open position in the list
+      trade_page.close_order()
+      trade_page.verify_order_notification_message('Close Order')
